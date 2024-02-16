cd0ae3d992ff551fc8824e2d7639c95af9984c47
Simone Sacchi <simone.sacchi@nubilaria.com>
2024-02-16 08:34:35 +0100

    refactor: rename poolQueries operation

--- mysql/features/runQueriesWithEndingConnection.js
+++ mysql/features/runQueriesWithEndingConnection.js
@@ -1,19 +1,15 @@
 "use strict";
 
-"use strict";
-
 const path = require("path");
 const process = require("process");
 const queryJob = require(path.resolve(process.cwd(), "mysql", "jobs", "query"));
-// const logJob = require(path.resolve(process.cwd(), "mysql", "jobs", "log"));
 const connection = require(path.resolve(process.cwd(), "mysql", "connections", "endingConnection"));
 
 module.exports = async function (howMany, sqlString) {
 
     for (let i = 0; i < howMany; i += 1) {
         const id = i + 1;
-        // logJob(id);
         await queryJob(connection, sqlString, [id]);
     }
 
-}
+};
--- mysql/features/runQueriesWithPoolConnection.js
+++ mysql/features/runQueriesWithPoolConnection.js
@@ -4,16 +4,14 @@
 
 const path = require("path");
 const process = require("process");
-const poolQueriesOperation = require(path.resolve(process.cwd(), "mysql", "operations", "poolQueries"));
+const runPoolQueriesOperation = require(path.resolve(process.cwd(), "mysql", "operations", "runPoolQueries"));
 const connection = require(path.resolve(process.cwd(), "mysql", "connections", "poolConnection"));
-// const logJob = require(path.resolve(process.cwd(), "mysql", "jobs", "log"));
 const connectionLimit = 10;
 
 module.exports = async function (howMany, sqlString) {
 
     for (let i = 0; i < howMany / 10; i += 1) {
-        // logJob((i + 1) * 10);
-        await poolQueriesOperation(connection, connectionLimit, sqlString, [i]);
+        await runPoolQueriesOperation(connection, connectionLimit, sqlString, [i]);
     }
 
 }
