ba6f1da353749edf9728a3dc8550d450085a0c1c
Simone Sacchi <simone.sacchi@nubilaria.com>
2024-02-19 15:38:51 +0100

    refactor: rename fuction

--- mysql/services/runQueriesWithBaseConnection.js
+++ mysql/services/runQueriesWithBaseConnection.js
@@ -3,11 +3,11 @@
 const path = require("path");
 const process = require("process");
 const constants = require(path.resolve(process.cwd(), "mysql", "constants"));
-const runQueriesJob = require(path.resolve(process.cwd(), "mysql", "features", "runQueries"));
+const runQueriesFeature = require(path.resolve(process.cwd(), "mysql", "features", "runQueries"));
 const connection = require(path.resolve(process.cwd(), "mysql", "connections", "baseConnection"));
 
 module.exports = async function (numberOfQueries = constants.NUMBER_OF_QUERIES) {
 
-    await runQueriesJob(connection, numberOfQueries, constants.SQL_STRING);
+    await runQueriesFeature(connection, numberOfQueries, constants.SQL_STRING);
 
 };
