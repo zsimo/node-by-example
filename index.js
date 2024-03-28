8a0588e65bc37eab150aab7267754bd9de9c84aa
Simone Sacchi <simone.sacchi@nubilaria.com>
2024-03-28 18:00:38 +0100

    feat: update index

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
 "use strict";
 
-
-
-
-const a = new Promise(function (res) {
-    setTimeout(function () {
-        res("ciao");
-    }, 1000);
-})
-
-a.then(function (result) {
-    console.log(result)
-    console.log(a);
-})
-console.log(a.status);
+console.log("ciao");
